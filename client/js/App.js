df8b3ee2f0eece0bdd0bcdbc6171cdae6f646cc2
Fiona <fyeung21@gmail.com>
2020-02-20 20:11:23 -0800

    set up file structure

---
+++ client/js/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>This app works</Text>
+    </>
+  );
+};
+
+
+
+export default App;
