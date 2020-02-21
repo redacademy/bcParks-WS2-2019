864dba3ef5aaf1bb5dc96b9b49a549c867131787
Miri <mirimarkovitzbl@gmail.com>
2020-02-21 11:13:15 -0800

    add activity screen and component

---
+++ client/js/components/TimePeriods.js
@@ -0,0 +1,19 @@
+import react, { Component } from 'react';
+
+class TimePeriods extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <Text>future buttons</Text>
+        )
+    };
+
+};
+
+export default TimePeriods;
---
+++ client/js/screens/Activity/ActivityContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Activity from './Activity';
+
+const ActivityContainer = ({ data }) => {
+
+    return (
+        <Activity data={data} />
+    )
+}
+
+export default ActivityContainer;
