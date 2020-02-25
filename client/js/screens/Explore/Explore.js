2dca119b42e93a1987e6c1ebd293bb8b9bc67e03
vancityjun <vancityjun@gmail.com>
2020-02-24 20:48:37 -0800

    added google maps screen

--- client/js/screens/Explore/Explore.js
+++ client/js/screens/Explore/Explore.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import {View, Text, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 const ExploreScreen = () => {
-
-    return (
-        <View>
-            <Text>Explore</Text>
-        </View>
-    );
-}
-
+  return (
+    <View>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 49.2479999,
+          longitude: -123.1300971,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}>
+        <Marker
+          coordinate={{
+            latitude: 49.2479999,
+            longitude: -123.1300971,
+          }}
+        />
+      </MapView>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 export default ExploreScreen;
