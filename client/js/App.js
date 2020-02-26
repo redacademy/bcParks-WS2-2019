f7473c4efb373d767d9bc6a1d75e325b6aa6c4ea
Hongsoon Kim <khsun6226@naver.com>
2020-02-25 21:09:37 -0800

    update schema and add apollo

--- client/js/App.js
+++ client/js/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import client from './config/api'
 import { Text } from 'react-native';
 import RootStackNavigator from './navigation/RootStackNavigator';
 
 const App = () => {
   return (
     <>
-      <RootStackNavigator />
+      <ApolloProvider client={client}>
+        <RootStackNavigator />
+      </ApolloProvider>
     </>
   );
 };
---
+++ client/js/config/api.js
@@ -0,0 +1,5 @@
+import ApolloClient from 'apollo-boost';
+
+export default new ApolloClient({
+    uri: "https://us1.prisma.sh/zareef-baksh/bcParks/dev"
+});
