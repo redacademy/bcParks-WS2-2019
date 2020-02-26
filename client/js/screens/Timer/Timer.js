6a610c68200f5ad6b2de868b42fca275a3835abe
Fiona <fyeung21@gmail.com>
2020-02-25 19:10:42 -0800

    set up timer screen

--- client/js/screens/Timer/Timer.js
+++ client/js/screens/Timer/Timer.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 const TimerScreen = () => {
 
+    const [startStop, setStartStop] = useState(true);
+
+    startStopPress = () => {
+        setStartStop(!startStop)
+    }
+
     return (
         <View>
-            <Text>Timer</Text>
+            <View>
+                <Text>pic</Text>
+            </View>
+            <View>
+                <Text>00:00:00</Text>
+                <Text>Spending more time in nature contributes to a better sleep cycle and helps in lowering anxiety</Text>
+                <TouchableOpacity onPress={this.startStopPress}>
+                    {startStop ?
+                        <Text>
+                            Start Green Time
+                        </Text> :
+                        <Text>
+                            Stop Green Time
+                        </Text>
+                    }
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
