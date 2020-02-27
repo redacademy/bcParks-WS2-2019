25e284116e42e1869a6a2594b4bcdb9d1c9a0929
Hongsoon Kim <khsun6226@naver.com>
2020-02-26 20:42:56 -0800

    install a package and add it in home screen

--- client/js/screens/Home/HomeContainer.js
+++ client/js/screens/Home/HomeContainer.js
@@ -1,13 +1,35 @@
 import React from "react"
-import { View } from "react-native"
+import { Text } from "react-native"
 import Home from "./Home"
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+
+const QUERY_PROGRESS = gql`
+    query {
+        progress(where:{id:1}) {
+            id
+            duration
+            completion
+        }
+    }
+`;
 
 const HomeContainer = () => {
-    return (
-        <View>
-            <Home />
-        </View>
-    )
+    const { loading, error, data } = useQuery(QUERY_PROGRESS);
+    if (loading) {
+        return (
+            <Text>Loading</Text>
+        )
+    } else if (error) {
+        return (
+            <Text>Error</Text>
+        )
+    } else {
+        return (
+            <Home data={data} />
+        )
+    }
 }
 
 export default HomeContainer
