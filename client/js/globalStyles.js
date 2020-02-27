7252817e165fac000735a2abcd7d626854b54adf
Fiona <fyeung21@gmail.com>
2020-02-27 00:51:34 -0800

    set up rest of theme and add to App.js

--- client/js/globalStyles.js
+++ client/js/globalStyles.js
@@ -1,11 +1,15 @@
 
-export const Theme = {
-    mainBkg: "#8CBE82",
-    mainText: "#000",
+export const theme = {
     primaryColor: "#49773A",
     accentColor: "#DA6645",
-    disabled: "#D3D3D3",
+    grayColor: "#767676",
+    screenBkgColor: "#8CBE82",
+    bodyTextColor: "#000",
+    disabledColor: "#9d9d9d",
     headlineFont: "Comfortaa-Regular",
-    bodyFont: "OpenSans-Regular"
+    bodyFont: "OpenSans-Regular",
+    h1FontSize: "34px",
+    h2FontSize: "28px",
+    bodyFontSize: "18px"
 }
 
