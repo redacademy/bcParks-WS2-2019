7c09da40bb2e3b667b7acc7155b924f9955abfab
Fiona <fyeung21@gmail.com>
2020-02-26 19:29:13 -0800

    set up theme colors

---
+++ client/js/globalStyles.js
@@ -0,0 +1,11 @@
+
+export const Theme = {
+    mainBkg: "#8CBE82",
+    mainText: "#000",
+    primaryColor: "#49773A",
+    accentColor: "#DA6645",
+    disabled: "#D3D3D3",
+    headlineFont: "Comfortaa-Regular",
+    bodyFont: "OpenSans-Regular"
+}
+
