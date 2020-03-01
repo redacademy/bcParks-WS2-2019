d87df0d5740a597ebf93c14f3dc072d15ddb9c34
Fiona <fyeung21@gmail.com>
2020-02-29 16:13:22 -0800

    add slider to mood screen

--- client/js/components/MoodSlider/MoodSlider.js
+++ client/js/components/MoodSlider/MoodSlider.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 import { MoodIcon } from './styles';
+import styled from 'styled-components';
+import { theme } from '../../globalStyles';
+
+const StyledSlider = styled.Slider`
+    width: 300px;
+    height: 100px;
+    margin: 0 auto 120px;
+`
 
 const MoodSlider = () => {
     return (
         <View>
             <MoodIcon>Face</MoodIcon>
-            <TouchableOpacity>
-                <Text>----slider---</Text>
-            </TouchableOpacity>
+            <StyledSlider
+                minimumValue={0}
+                maximumValue={5}
+                minimumTrackTintColor={theme.accentColor}
+                maximumTrackTintColor="#FFF"
+            />
         </View>
     );
 }
