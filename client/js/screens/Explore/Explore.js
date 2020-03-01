2a796d6ef3c0e6e99b68d4619c071f9f992adf3f
vancityjun <vancityjun@gmail.com>
2020-02-29 16:55:15 -0800

    logic to fetch data

--- client/js/screens/Explore/Explore.js
+++ client/js/screens/Explore/Explore.js
@@ -1,24 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import fetchData from '../../config/fetchData';
 import MapSwiper from '../../components/MapSwiper';
-import {ADD_MAP} from './helper/mutation';
-import {useMutation} from '@apollo/react-hooks';
 
-const GOOGLE_API_KEY = 'AIzaSyBAD3HSvKDHWvLVSEXw9hy4ruEhSrpfA1k';
 const dataURL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=49.2479999,-123.1300971&radius=1500&type=park&fields=place_id,name,opening_hours,formatted_address,geometry&key=${GOOGLE_API_KEY}`;
 const ExploreScreen = () => {
-  const [mapData, setMapData] = useState();
-  const getImages = reference => {
-    const photoURL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${reference}&key=${GOOGLE_API_KEY}`;
-  };
-  const addMap = useMutation(ADD_MAP);
-  // addMap({variables: {type: mapData}});
+  const [mapData, setMapData] = useState([]);
+
+  const query = `query {
+    maps {
+      id
+      name
+      externalId
+      vicinity
+      geometry {
+        location {
+          lat
+          lng
+        }
+      }
+    }
+  }`;
+
   useEffect(() => {
-    fetch(dataURL)
-      .then(response => response.json())
-      .then(data => setMapData(data.results));
+    fetchData(query).then(data => {
+      setMapData(data.maps);
+    });
   }, []);
+  const getMarkers = () => {
+    mapData.map(map => {
+      <Marker
+        coordinate={{
+          latitude: map.geometry.location.lat,
+          longitude: map.geometry.location.lng,
+        }}
+        title={map.name}
+        key={map.id}
+      />;
+    });
+  };
   return (
     <View>
       <MapView
@@ -31,12 +52,7 @@ const ExploreScreen = () => {
           longitudeDelta: 0.0421,
         }}
         showsUserLocation={true}>
-        <Marker
-          coordinate={{
-            latitude: 49.2479999,
-            longitude: -123.1300971,
-          }}
-        />
+        {mapData.length ? getMarkers() : null}
       </MapView>
       <MapSwiper />
     </View>
