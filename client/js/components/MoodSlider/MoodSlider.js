d589723e766c8f3b4253ef43ead0fb70f9e5f282
Fiona <fyeung21@gmail.com>
2020-02-29 16:58:26 -0800

    adjust settings on slider

--- client/js/components/MoodSlider/MoodSlider.js
+++ client/js/components/MoodSlider/MoodSlider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { MoodIcon } from './styles';
 import styled from 'styled-components';
@@ -6,19 +6,28 @@ import { theme } from '../../globalStyles';
 
 const StyledSlider = styled.Slider`
     width: 300px;
-    height: 100px;
     margin: 0 auto 120px;
 `
 
 const MoodSlider = () => {
+    const [changeFace, setChangeFace] = useState(0);
+
+    changeFaceSlide = () => {
+        setChangeFace(changeFace + 0.5)
+    }
+
     return (
         <View>
-            <MoodIcon>Face</MoodIcon>
+            <MoodIcon>Face test:{changeFace}</MoodIcon>
             <StyledSlider
+                step={0.5}
+                value={2.5}
                 minimumValue={0}
                 maximumValue={5}
+                onValueChange={this.changeFaceSlide}
+                thumbTintColor={theme.accentColor}
                 minimumTrackTintColor={theme.accentColor}
-                maximumTrackTintColor="#FFF"
+                maximumTrackTintColor={theme.invertTextColor}
             />
         </View>
     );
