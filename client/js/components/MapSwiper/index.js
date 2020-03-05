73767b887b3059e2fefdb3c9d814418e9bd7ec3d
vancityjun <vancityjun@gmail.com>
2020-03-04 20:00:49 -0800

    add mutation, carousel feature for maps

--- client/js/components/MapSwiper/index.js
+++ client/js/components/MapSwiper/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, Animated, ScrollView} from 'react-native';
 import Card from '../Card';
 import styled from 'styled-components';
 
@@ -7,11 +7,31 @@ const Box = styled.View`
   padding: 15px;
 `;
 const MapSwiper = ({mapData}) => {
+  const [animation, setAnimation] = useState(new Animated.Value(0));
+  const cardWidth = 291;
   const Slides = () => mapData.map(map => <Card detail={map} key={map.id} />);
   return (
     <Box>
       <Text>Near By Green Spaces</Text>
-      {Slides()}
+      <Animated.ScrollView
+        horizontal
+        scrollEventThrottle={1}
+        showsHorizontalScrollIndicator={false}
+        snapToInterval={cardWidth}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: animation,
+                },
+              },
+            },
+          ],
+          {useNativeDriver: true},
+        )}>
+        {Slides()}
+      </Animated.ScrollView>
     </Box>
   );
 };
