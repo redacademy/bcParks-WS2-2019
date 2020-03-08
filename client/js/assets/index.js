4ecc9adc732cc7b22b9f31dcf7308ca54d8be400
Fiona <fyeung21@gmail.com>
2020-03-07 17:00:12 -0800

    install svg transformer dependency and add images as components

---
+++ client/js/assets/index.js
@@ -0,0 +1,23 @@
+
+export const OnboardingLanding = require('./images/OnboardingLanding.svg');
+export const OnboardingLocation = require('./images/OnboardingLocation.svg');
+export const OnboardingTimer = require('./images/OnboardingTimer.svg');
+export const OnboardingEnd = require('./images/OnboardingEnd.svg');
+
+export const Timer = require('./images/Timer.svg');
+export const Activity = require('./images/Activity.svg');
+export const Explore = require('./images/Explore.svg');
+
+export const MoodVerySad = require('./images/MoodVerySad.svg');
+export const MoodSad = require('./images/MoodSad.svg');
+export const MoodNeutral = require('./images/MoodNeutral.svg');
+export const MoodHappy = require('./images/MoodHappy.svg');
+export const MoodVeryHappy = require('./images/MoodVeryHappy.svg');
+
+export const LeafSmall = require('./images/LeafSmall.svg');
+export const LeafLarge = require('./images/leafLarge.svg');
+
+export const TreeLeft = require('./images/TreeLeft.svg');
+export const TreeRight = require('./images/TreeRight.svg');
+
+export const Background = require('./images/Background.svg');
