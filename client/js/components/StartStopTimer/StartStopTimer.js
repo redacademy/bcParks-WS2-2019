15f9727ed1f6f16047b293e4d73ea024eba32b5b
Hongsoon Kim <khsun6226@naver.com>
2020-03-09 21:06:09 -0700

    add mutation for timer

--- client/js/components/StartStopTimer/StartStopTimer.js
+++ client/js/components/StartStopTimer/StartStopTimer.js
@@ -2,14 +2,54 @@ import React, { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { StartStopCont, TimerDisplay, InfoTextCont, InfoText, Btn } from './styles.js';
 import { theme } from '../../globalStyles';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+import moment from 'moment';
+
+const Mutation_CreateSession = gql`
+    mutation CreateSession($timeStart: DateTime!, $timeEnd: DateTime!, $date: DateTime!) {
+        createSession(
+            data:{
+                timeStart: $timeStart, 
+                timeEnd: $timeEnd,
+                date: $date
+            }
+        ){
+        id
+        timeEnd
+        timeStart
+        date
+        }
+    }
+`
 
 const StartStopTimer = () => {
 
     const [startStop, setStartStop] = useState(true);
+    const [startTime, setStartTime] = useState("");
+    const [endTime, setEndTime] = useState("");
+    const [date, setDate] = useState("");
 
-    startStopPress = () => {
-        setStartStop(!startStop)
+    const [CreateSession] = useMutation(Mutation_CreateSession);
+    const create = () => {CreateSession({
+                        variables: {
+                            timeStart: startTime,
+                            timeEnd: endTime,
+                            date: date
+                        }
+                    })}
+
+    const startTimer = () => {
+        setStartStop(!startStop);
+        setStartTime(moment().format());
+        setDate(moment().format("YYYY-MM-DD"));
     }
+
+    const stopTimer = () => {
+        setStartStop(!startStop);
+        setEndTime(moment().format());
+    }
+
 
     return (
         <StartStopCont>
@@ -17,7 +57,12 @@ const StartStopTimer = () => {
             <InfoTextCont>
                 <InfoText>Spending more time in nature contributes to a better sleep cycle and helps with lowering anxiety</InfoText>
             </InfoTextCont>
-            <TouchableOpacity onPress={this.startStopPress}>
+            <TouchableOpacity onPress={ 
+                startStop ? ()=>{
+                startTimer()} : ()=> {
+                stopTimer();
+
+            }}>
                 {startStop ?
                     <Btn isStart theme={theme}>
                         Start Green Time
