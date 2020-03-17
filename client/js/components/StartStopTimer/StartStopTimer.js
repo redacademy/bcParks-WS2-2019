acf7e19635cda72d30d31d66c7398df7f1d5ae87
Hongsoon Kim <khsun6226@naver.com>
2020-03-16 23:58:59 -0700

    finish Timer Function

--- client/js/components/StartStopTimer/StartStopTimer.js
+++ client/js/components/StartStopTimer/StartStopTimer.js
@@ -1,17 +1,83 @@
-import React, { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
-import { StartStopCont, TimerDisplay, InfoTextCont, InfoText, BtnCont } from './styles.js';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import { StartStopCont, TimerDisplay, TimerContainer, InfoTextCont, InfoText, BtnCont } from './styles.js';
 import { theme, PrimaryBtn } from '../../globalStyles';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 import moment from 'moment';
 
+const Query_Session = gql`
+    query {
+        session(
+            where: {
+                id: "ck7o67opa0j87094270k3z1np"
+            }
+        ){
+            id
+            timeStart
+            timeEnd
+            date
+        }
+    }
+`
 
-const StartStopTimer = ({navigation}) => {
+const StartStopTimer = ({ navigation }) => {
 
     const [startStop, setStartStop] = useState(true);
     const [startTime, setStartTime] = useState("");
     const [date, setDate] = useState("");
+
+    // const { data, loading, error } = useQuery(Query_Session);
+
+    const StopWatch = (time) => {
+        const calculateTimeLeft = () => {
+            const diff = +new Date() - time.time;
+
+            let timeLeft = {};
+
+            if (diff > 0) {
+                timeLeft = {
+                    hours: Math.floor((diff / (1000 * 60 * 60)) % 24),
+                    minutes: Math.floor((diff / 1000 / 60) % 60),
+                    seconds: Math.floor((diff / 1000) % 60)
+                }
+            }
+            return timeLeft;
+        };
+
+        const [timer, setTimer] = useState({
+            hours: 0,
+            minutes: 0,
+            seconds: 0
+        });
+
+        useEffect(() => {
+            let mounted = true;
+            if (mounted) {
+                setTimeout(() => {
+                    setTimer(calculateTimeLeft());
+                }, 1000)
+            }
+        })
+
+        const timerComponents = [];
+
+        Object.keys(timer).forEach(interval => {
+            if (interval === 'seconds') {
+                timerComponents.push(
+                    <TimerDisplay>{addZero(timer[interval])}</TimerDisplay>
+                )
+            } else {
+                timerComponents.push(
+                    <TimerDisplay>{addZero(timer[interval])}:</TimerDisplay>
+                )
+            }
+        })
+
+        return (
+            timerComponents
+        )
+    }
 
     const startTimer = () => {
         setStartStop(!startStop);
@@ -22,37 +88,50 @@ const StartStopTimer = ({navigation}) => {
     const stopTimer = () => {
         setStartStop(!startStop);
         const endTime = moment().format()
-        navigation.push('MoodSelect', {startTime, endTime, date})
+        navigation.push('MoodSelect', { startTime, endTime, date })
+
     }
 
-
+    const addZero = (num) => (
+        num <= 9 ? `0${num}` : num
+    )
     return (
         <StartStopCont>
-            <TimerDisplay>00:00:00</TimerDisplay>
+            <TimerContainer>
+                {startStop ?
+                    <TimerDisplay>00:00:00</TimerDisplay> :
+                    <StopWatch time={parseInt(moment().format('x'))} />}
+            </TimerContainer>
+            {/* <TouchableOpacity onPress={() => {
+                    const session = data.session;
+                    let diff = new Date(session.timeEnd) - new Date(session.timeStart);
+                    setTime(diff / 3600000)
+                    console.log(time)
+                }}>
+                    <Text>Log</Text>
+                </TouchableOpacity> */}
             <InfoTextCont>
                 <InfoText>Spending more time in nature contributes to a better sleep cycle and helps with lowering anxiety</InfoText>
             </InfoTextCont>
             <BtnCont>
-                <TouchableOpacity onPress={
-                    startStop ? () => {
-                        startTimer()
-                    } : () => {
-                        stopTimer();
-
-                    }}>
+                <TouchableOpacity onPress={() => {
+                    startStop ?
+                        startTimer() :
+                        stopTimer()
+                }}>
                     {startStop ?
                         <PrimaryBtn>
                             Start Green Time
-                    </PrimaryBtn>
-                        :
+                        </PrimaryBtn> :
                         <PrimaryBtn isStop theme={theme}>
                             Stop Green Time
-                    </PrimaryBtn>
+                        </PrimaryBtn>
                     }
                 </TouchableOpacity>
             </BtnCont>
         </StartStopCont>
     );
+
 }
 
 export default StartStopTimer;
