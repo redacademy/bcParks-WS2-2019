10ed2ea4d1a6125621decbe5fce62806bbedce07
Fiona <fyeung21@gmail.com>
2020-03-19 20:34:16 -0700

    sketch initial dots

--- client/js/components/DotNav/DotNav.js
+++ client/js/components/DotNav/DotNav.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import { theme } from '../../globalStyles';
+
+export const DotsCont = styled.View`
+    display: flex;
+    flex-direction: row;
+`
+export const Dot = styled.Text`
+    background-color: ${props => props.isActive ? theme.accentColor : theme.invertTxtColor};
+    text-align: center;
+    padding: 10px;
+    width: 100%;
+    border-radius: 25px;
+    overflow:hidden;
+`
 
 const DotNav = () => {
-
-}
+    return (
+        <DotsCont>
+            <Dot isActive> </Dot>
+            <Dot> </Dot>
+        </DotsCont>
+    )
+};
 
 export default DotNav;
