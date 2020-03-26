a25343948bb6942a9d521b8d685597e128e95a56
vancityjun <vancityjun@gmail.com>
2020-03-25 17:19:42 -0700

    improve styles, add animation for circle progress bar

--- client/js/screens/Home/styles.js
+++ client/js/screens/Home/styles.js
@@ -1,46 +1,48 @@
 import styled from 'styled-components';
-import { theme } from '../../globalStyles';
+import {theme} from '../../globalStyles';
 
 export const DetailedProgressContainer = styled.View`
-    display: flex;
-    align-items: center;
-    margin: 0 auto 160px;
-    
-`
+  display: flex;
+  align-items: center;
+  margin: 0 auto 160px;
+`;
 export const HomeHeaderCont = styled.View`
-    margin: 25px auto;
-`
+  margin: 25px auto;
+`;
 export const ProgressBarContainer = styled.View`
-    display: flex;
-    flex-direction: row;
-`
+  display: flex;
+  flex-direction: row;
+`;
 export const Flex = styled.View`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 30px;
+  width: 100%;
+`;
 export const HomeText = styled.Text`
-    font-family: ${theme.bodyFont};
-    font-size: ${theme.bodyFontSize};
-    color: ${theme.bodyTextColor};
-`
+  font-family: ${theme.bodyFont};
+  font-size: ${theme.bodyFontSize};
+  color: ${theme.bodyTextColor};
+`;
 
 export const ProgressText = styled.Text`
-    font-family: ${theme.bodyFont};
-    font-size: ${props => props.isNumber ? `44px` : theme.bodyFontSize};
-    color: ${theme.bodyTextColor};
-`
+  font-family: ${theme.bodyFont};
+  font-size: ${props => (props.isNumber ? `44px` : theme.bodyFontSize)};
+  color: ${theme.bodyTextColor};
+`;
 export const Complete = styled.View`
-    margin-right: 15px;
-    width: 90px;
-    height: 160px;
-    align-items: center;
-    justify-content: center;
-`
+  margin-right: 15px;
+  width: 90px;
+  height: 160px;
+  align-items: center;
+  justify-content: center;
+`;
 export const Goal = styled.View`
-    margin-left: 15px;
-    width: 90px;
-    height: 160px;
-    align-items: center;
-    justify-content: center
-`
+  margin-left: 15px;
+  width: 90px;
+  height: 160px;
+  align-items: center;
+  justify-content: center;
+`;
